4ba075650dee6d3f22531b2ea244e8d0dd981fb6
Kadir Egilmez <kegilmez@kadirs-mbp.home>
2020-04-18 14:27:52 +0100

    add clearItem and updateItem reducer

--- client/src/state/reducers/itemReducer.js
+++ client/src/state/reducers/itemReducer.js
@@ -1,6 +1,8 @@
 import {
   GET_ITEMS,
+  CLEAR_ITEMS,
   DELETE_ITEM,
+  UPDATE_ITEM,
   ADD_ITEM,
   ITEMS_ERROR,
 } from '../actions/types';
@@ -13,10 +15,22 @@ export default (state, action) => {
         isLoading: false,
         items: action.payload,
       };
+    case CLEAR_ITEMS:
+      return {
+        ...state,
+        items: [],
+      };
     case DELETE_ITEM:
       return {
         ...state,
         items: state.items.filter((item) => item._id !== action.payload),
+      };
+    case UPDATE_ITEM:
+      return {
+        ...state,
+        items: state.items.map((item) =>
+          item._id === action.payload._id ? action.payload : item
+        ),
       };
     case ADD_ITEM:
       return {
